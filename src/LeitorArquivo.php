47363295b3c35f0ac045689c34b83014a04b9d7f
Andressa Colla <40358278+andressaColla@users.noreply.github.com>
2020-10-01 14:33:24 -0300

    finished exam

--- src/LeitorArquivo.php
+++ src/LeitorArquivo.php
@@ -3,6 +3,7 @@
 
 namespace App;
 
+use Exception;
 
 class LeitorArquivo
 {
@@ -11,17 +12,41 @@ class LeitorArquivo
     {
         if(!file_exists($caminhoArquivo)) {
             throw new \InvalidArgumentException("O caminho do arquivo é inválido!");
+        } else {
+            $this->caminhoArquivo = $caminhoArquivo;
         }
 
     }
 
     public function getColunas(): array
     {
-        return [];
+        $linhas = file($this->caminhoArquivo);
+        foreach ($linhas as $linha) {
+            $coluna = explode('  ', trim($linha));
+            break;
+        }
+        return $coluna;
+        
     }
 
-    public function getValorLinhaColuna(int $numeroLinha, string $nomeColuna): string
+    public function getValorLinhaColuna(int $numeroLinha, string $nomeColuna): string //0 ID
     {
-        return "";
+        $colunas = $this->getColunas();
+        $numeroColuna = array_search($nomeColuna, $colunas);
+        if ($numeroColuna === false) {
+            throw new \InvalidArgumentException("Coluna inexistente");
+        }
+        $linhas = file($this->caminhoArquivo);
+        array_shift($linhas);
+        $linhaProcurada = explode("  ", $linhas[$numeroColuna]);
+
+        try {
+            $linhaEncontrada = $linhaProcurada[$numeroLinha];
+        } catch(Exception $e) {
+            throw new \InvalidArgumentException("Linha inexistente");
+
+        }
+
+        return $linhaProcurada[$numeroLinha];
     }
 }
--- src/ValidarRotaService.php
+++ src/ValidarRotaService.php
@@ -3,12 +3,62 @@
 
 namespace App;
 
+use Exception;
 
 class ValidarRotaService
 {
     //TODO vc deve implementar esse método
     public function isValida(array $siglasEstados)
     {
+        $estados = [
+            'AC'=>['AC' => 1, 'AM' => 1, 'RO' => 1],
+            'AL'=>['AL' => 1, 'SE' => 1, 'PE' => 1, 'BA' => 1],
+            'AP'=> ['AP' => 1, 'PA' => 1],
+            'AM'=> ['AM' => 1, 'AC' => 1, 'RO' => 1, 'MT' => 1, 'PA' => 1, 'RR' => 1],
+            'BA'=> ['BA' => 1, 'ES' => 1, 'MG' => 1, 'GO' => 1, 'TO' => 1, 'PI' => 1, 'PE' => 1, 'AL' => 1, 'SE' => 1],
+            'CE'=> ['CE' => 1, 'PI' => 1, 'PE' => 1, 'PB' => 1, 'RN' => 1],
+            'ES'=> ['ES' => 1, 'RJ' => 1, 'MG' => 1, 'BA' => 1],
+            'GO'=> ['GO' => 1, 'MG' => 1, 'BA' => 1, 'TO' => 1, 'MT' => 1, 'MS' => 1],
+            'MA'=> ['MA' => 1, 'PI' => 1, 'TO' => 1, 'PA' => 1],
+            'MT'=> ['MT' => 1, 'MS' => 1, 'GO' => 1, 'TO' => 1, 'PA' => 1, 'AM' => 1, 'RO' => 1],
+            'MS'=> ['MS' => 1, 'PR' => 1, 'SP' => 1, 'MG' => 1, 'GO' => 1, 'MT' => 1],
+            'MG'=> ['MG' => 1, 'SP' => 1, 'RJ' => 1, 'ES' => 1, 'BA' => 1, 'GO' => 1, 'MS' => 1],
+            'PA'=> ['PA' => 1, 'MT' => 1, 'TO' => 1, 'AP' => 1, 'AM' => 1,],
+            'PB'=> ['PB' => 1, 'PE' => 1, 'RN' => 1, 'CE' => 1],
+            'PR'=> ['PR' => 1, 'SC' => 1, 'MS' => 1, 'SP' => 1],
+            'PE'=> ['PE' => 1, 'BA' => 1, 'AL' => 1, 'PB' => 1, 'CE' => 1, 'PI' => 1, ],
+            'PI'=> ['PI' => 1, 'BA' => 1, 'PE' => 1, 'CE' => 1, 'MA' => 1, 'TO' => 1],
+            'RJ'=> ['RJ' => 1, 'SP' => 1, 'ES' => 1, 'MG' => 1],
+            'RN'=> ['RN' => 1, 'PB' => 1, 'CE' => 1],
+            'RS'=> ['RS' => 1, 'SC' => 1],
+            'RO'=> ['RO' => 1, 'AM' => 1, 'AC' => 1, 'MT' => 1],
+            'RR'=> ['RR' => 1, 'AM' => 1, 'PA' => 1],
+            'SC'=> ['SC' => 1, 'RS' => 1, 'PR' => 1],
+            'SP'=> ['SP' => 1, 'PR' => 1, 'MS' => 1, 'MG' => 1, 'RJ' => 1],
+            'SE'=> ['SE' => 1, 'AL' => 1, 'BA' => 1],
+            'TO'=> ['TO' => 1, 'MT' => 1, 'GO' => 1, 'BA' => 1, 'PI' => 1, 'MA' => 1, 'PA' => 1]
+        ];
 
+        if (count($siglasEstados) <= 1) {
+            return false;
+        }
+
+        $count = 1;
+        $totalSiglas = count($siglasEstados);
+        $sigla = 0;
+
+        while ($count < $totalSiglas) {
+            try {
+                if ($estados[$siglasEstados[$sigla]][$siglasEstados[$sigla + 1]] == 1) {
+                    $sigla++;
+                }
+            } catch (Exception $e) {
+                return false;
+            }
+
+            $count++;
+        }
+        return true;
+       
     }
 }
